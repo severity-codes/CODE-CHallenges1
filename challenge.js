679a4d92f51dd1a8a36c646a195627d3204450be
Severity Codes <serenitysmithrn@hotmail.com>
2024-09-09 22:36:32 -0400

    .

---
+++ challenge.js
@@ -0,0 +1,15 @@
+function bitwiseAND(n1, n2) {
+  return n1 & n2;
+}
+
+function bitwiseOR(n1, n2) {
+  return n1 | n2;
+}
+
+function bitwiseXOR(n1, n2) {
+  return n1 ^ n2;
+}
+
+console.log(bitwiseAND(7, 12));
+console.log(bitwiseOR(8, 7));
+console.log(bitwiseXOR(10, 11));
